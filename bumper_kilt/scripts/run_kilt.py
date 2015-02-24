268139d19f6c349eae6094c59eb886e538d824b5
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-02-24 17:11:07 +0000

    Complete bumper kilt by adding publisher

--- bumper_kilt/scripts/run_kilt.py
+++ bumper_kilt/scripts/run_kilt.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
+
+import os
 import time
 import serial
-# configure the serial connections (the parameters differs on the
-# device you are connecting to)
+import copy
+
+import rospy
+from std_msgs.msg import Int32MultiArray
 
 global DEBUG
-DEBUG = 1
+DEBUG = 0
 
 def dbprint(s, newline=True):
     global DEBUG
@@ -20,12 +24,23 @@ def dbprint(s, newline=True):
 class Bumper(object):
 
     def __init__(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        self.robot = os.environ["ROBOT_NAME"]
+        self.nodename = rospy.get_name()
+        rospy.loginfo("Starting bumper_kilt node", self.nodename)
+        self.pub = rospy.Publisher(self.robot + '/bumper_kilt',
+                              Int32MultiArray,
+                              queue_size=10)
+        rospy.init_node("bumper_kilt_node")
+        self.rate = rospy.Rate(10)
+
+        self.bits = []
         self.ser = serial.Serial(
             port="/dev/ttyS0",
             baudrate=38400,
-            parity=serial.PARITY_ODD,
+            parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.SEVENBITS
+            bytesize=serial.EIGHTBITS
             )
         try:
             print("Opening port...")
@@ -49,14 +64,88 @@ class Bumper(object):
         else:
             print("DONE")
 
-    def get_sensors(self, msg):
+    def get_sensor_data(self, msg):
         n_bits = 6
         h2b = lambda x: bin(int(x, 16))[2:].zfill(n_bits)
         dbprint(msg)
-        bits = [h2b(msg[i:i+2]) for i in range(0, len(msg), 2)]
-        dbprint("Single bits: " + str(bits))
+        self.bits = [h2b(msg[i:i+2]) for i in range(0, len(msg), 2)]
+        dbprint("Single bits: " + str(self.bits))
+
+    def bumped_north(self):
+        b3 = self.bits[2]
+        b4 = self.bits[3]
+        return int(b4[-1]) or int(b3[-1]) or int(b3[-2]) or int(b4[-2])
+
+    def bumped_north_east(self):
+        b3 = self.bits[2]
+        return int(b3[3]) or int(b3[2])
+
+    def bumped_east(self):
+        b3 = self.bits[2]
+        b1 = self.bits[0]
+        return int(b3[1]) or int(b3[0]) or int(b1[0]) or int(b1[1])
+
+    def bumped_south_east(self):
+        b1 = self.bits[0]
+        return int(b1[3]) or int(b1[2])
+
+    def bumped_south(self):
+        b1 = self.bits[0]
+        b2 = self.bits[1]
+        return int(b1[-2]) or int(b1[-1]) or int(b2[-1]) or int(b2[-2])
+
+    def bumped_south_west(self):
+        b2 = self.bits[1]
+        return int(b2[3]) or int(b2[2])
+
+    def bumped_west(self):
+        b2 = self.bits[1]
+        b4 = self.bits[3]
+        return int(b2[1]) or int(b2[0]) or int(b4[0]) or int(b4[1])
+
+    def bumped_north_west(self):
+        b4 = self.bits[3]
+        return int(b4[3]) or int(b4[2])
+
+    def get_bumps(self):
+        bumps = []
+        bits = copy.deepcopy(self.bits)
+        if self.bumped_north():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        if self.bumped_north_east():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        if self.bumped_east():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        if self.bumped_south_east():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        if self.bumped_south():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        if self.bumped_south_west():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        if self.bumped_west():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        if self.bumped_north_west():
+            bumps.append(1)
+        else:
+            bumps.append(0)
+        self.bumps = bumps
 
     def auto_loop(self):
+        counter = 0
         while True:
             out = ""
             time.sleep(0.05)
@@ -65,16 +154,22 @@ class Bumper(object):
                 out += r
             if len(out) == 12:
                 dbprint(str([o for o in out]))
-                print("New message: " + out[:-2])
-                s = self.get_sensors(out[2:-2])
-                print s
-
+                print(str(counter) + " - New message: " + out[:-2])
+                self.get_sensor_data(out[2:-2])
+                self.get_bumps()
+                counter += 1
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print("CURRENTLY TOUCHING: " + str(self.bumps))
+                msg = Int32MultiArray()
+                for e in self.bumps:
+                    msg.data.append(e)
+                self.pub.publish(msg)
+                self.rate.sleep()
 
 def main():
     b = Bumper()
     b.set_auto()
     b.auto_loop()
-
 
 
 if __name__ == "__main__":
