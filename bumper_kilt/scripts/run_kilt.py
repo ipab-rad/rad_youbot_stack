f1ff4b28405984a73bfe4e1d987a043a9871641b
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-02-24 15:17:20 +0000

    Get bit data from bumpers

--- bumper_kilt/scripts/run_kilt.py
+++ bumper_kilt/scripts/run_kilt.py
@@ -1,57 +1,79 @@
 #!/usr/bin/python
 import time
 import serial
-
 # configure the serial connections (the parameters differs on the
 # device you are connecting to)
+
+global DEBUG
+DEBUG = 1
+
+def dbprint(s, newline=True):
+    global DEBUG
+    if not DEBUG:
+        pass
+    if newline:
+        print(s)
+    else:
+        print(s),
+
 
 class Bumper(object):
 
     def __init__(self):
-        try:
-            self.ser = serial.Serial(
-                port="/dev/ttyS0",
-                baudrate=38400,
-                parity=serial.PARITY_ODD,
-                stopbits=serial.STOPBITS_ONE,
-                bytesize=serial.EIGHTBITS
+        self.ser = serial.Serial(
+            port="/dev/ttyS0",
+            baudrate=38400,
+            parity=serial.PARITY_ODD,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.SEVENBITS
             )
-        except Exception:
-            print("Bad initialisation! Check the configuration of "
-                  "the serial port!")
-            exit()
-        self.ser.open()
-        self.ser.isOpen()
+        try:
+            print("Opening port...")
+            self.ser.open()
+        except:
+            print("Port is already open!")
+            self.ser.isOpen()
 
-    def loop(self):
-        input=1
-        while 1 :
-            # get keyboard input
-            input = raw_input(">> ")
-            # Python 3 users
-            # input = input(">> ")
-            if input == "exit":
-                self.ser.close()
-                exit()
-            else:
-                # send the character to the device
-                # (note that I happend a \r\n carriage return and line feed to
-                # the characters - this is requested by my device)
-                self.ser.write(input + "\r\n")
-                out = ""
-                # let's wait one second before reading output (let's give
-                # device time to answer)
-                time.sleep(1)
-                while self.ser.inWaiting() > 0:
-                    out += self.ser.read(1)
+    def set_auto(self):
+        print("Setting mode to auto... "),
+        self.ser.write('a')
+        out = ""
+        time.sleep(1)
+        while self.ser.inWaiting() > 0:
+            r = self.ser.read(1)
+            out += r
+        if str(out) != "AUTO_MODE\r\n":
+            print ""
+            print "MSG: " + [o for o in out]
+            raise Exception("Could not set mode to auto!")
+        else:
+            print("DONE")
 
-                if out != "":
-                    print ">> " + out
+    def get_sensors(self, msg):
+        n_bits = 6
+        h2b = lambda x: bin(int(x, 16))[2:].zfill(n_bits)
+        dbprint(msg)
+        bits = [h2b(msg[i:i+2]) for i in range(0, len(msg), 2)]
+        dbprint("Single bits: " + str(bits))
+
+    def auto_loop(self):
+        while True:
+            out = ""
+            time.sleep(0.05)
+            while self.ser.inWaiting() > 0:
+                r = self.ser.read(1)
+                out += r
+            if len(out) == 12:
+                dbprint(str([o for o in out]))
+                print("New message: " + out[:-2])
+                s = self.get_sensors(out[2:-2])
+                print s
 
 
 def main():
     b = Bumper()
-    b.loop()
+    b.set_auto()
+    b.auto_loop()
 
 
 
